2bfbbd5eb879edd7c7b29a4ffa6efd9ff89fd66b
RamazonovDeveloper <ramazonovferuz01@gmail.com>
2023-10-13 10:12:36 +0500

    Home page updates for 13.10.2023

--- pages/home/index.js
+++ pages/home/index.js
@@ -6,36 +6,10 @@ import HomeDocuments from "../../views/home/documents/homeDocuments";
 import HomeTransactions from "../../views/home/transactions/homeTransactions";
 
 export default function HomePage() {
-  // const [dateFilter, setDateFilter] = useState(false);
-  // const [typeTransaction, setTypeTransaction] = useState(false);
-
-  // useEffect(() => {
-  //   window.addEventListener("click", function(event) {
-  //     console.log(event && event?.target.className == 'my_profile_image');
-  //     if ((event && event?.target.className) == 'my_profile_image') {
-  //       console.log("eeeee salom dostlar");
-  //     }
-  //     // if(!event.target.className(".home_navbar_toggle_image")){
-  //     //   setAccountToggleActive(!accountToggleActive)
-  //     // }
-  //   })
-
-  //   // return () => {
-  //   //   window.removeEventListener("click", function(event) {
-  //   //     console.log(event && event?.target.className == 'my_profile_image');
-
-  //   //     // if(!event.target.className(".home_navbar_toggle_image")){
-  //   //     //   setAccountToggleActive(!accountToggleActive)
-  //   //     // }
-  //   //   })
-  //   // }
-
-  // }, []);
 
   return (
     <div className={styles.home}>
-      {/* MAIN SECTION BTNS */}
-
+      
       <HomeHeader />
 
       <div className={styles.home_main}>
